36a717b3421952d6465c9a2fc03974b4d39e77d0
haupt <sha1-4fbabc136ee8040d6e1d9aecae17f6aec6c30276@cern.ch>
2008-11-24 15:38:47 +0000

    Updates for SBS pass/fail, having multiple probes, electrons greater than eta=3

--- test/Electron_EDM_Ntuple_cfg.py
+++ test/Electron_EDM_Ntuple_cfg.py
@@ -27,30 +27,33 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 
 process.TPEdm = cms.EDProducer("TagProbeEDMNtuple",
-    allProbeTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("SuperClustersMatch"), cms.InputTag("GsfElectronsMatch"), cms.InputTag("IsolationMatch"), cms.InputTag("IdMatch")),
+    allProbeTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("SuperClustersMatch"), cms.InputTag("GsfElectronsMatch"), cms.InputTag("IsolationMatch"), cms.InputTag("IdMatch"), cms.InputTag("HFSCMatch")),
     checkExactOverlap = cms.untracked.bool(False),
     triggerDelRMatch = cms.untracked.double(0.3),
-    allProbeCandTags = cms.untracked.VInputTag(cms.InputTag("theSuperClusters"), cms.InputTag("theGsfElectrons"), cms.InputTag("theIsolation"), cms.InputTag("theId")),
+    triggerDelPtRelMatch = cms.untracked.double(0.3),
+    allProbeCandTags = cms.untracked.VInputTag(cms.InputTag("theSuperClusters"), cms.InputTag("theGsfElectrons"), cms.InputTag("theIsolation"), cms.InputTag("theId"), cms.InputTag("theHFSuperClusters")),
     # Truth Matching tags
-    passProbeTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("GsfElectronsMatch"), cms.InputTag("IsolationMatch"), cms.InputTag("IdMatch"), cms.InputTag("HLTMatch")),
+    passProbeTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("GsfElectronsMatch"), cms.InputTag("IsolationMatch"), cms.InputTag("IdMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HFIDMatch")),
     # Tag & Probe Electron Candidate Collections
-    tagCandTags = cms.untracked.VInputTag(cms.InputTag("theHLT"), cms.InputTag("theHLT"), cms.InputTag("theHLT"), cms.InputTag("theHLT")),
+    tagCandTags = cms.untracked.VInputTag(cms.InputTag("theHLT"), cms.InputTag("theHLT"), cms.InputTag("theHLT"), cms.InputTag("theHLT"), cms.InputTag("theHLT")),
     # Tag & Probe Muon Association Map 
-    tagProbeMapTags = cms.untracked.VInputTag(cms.InputTag("tpMapSuperClusters"), cms.InputTag("tpMapGsfElectrons"), cms.InputTag("tpMapIsolation"), cms.InputTag("tpMapId")),
+    tagProbeMapTags = cms.untracked.VInputTag(cms.InputTag("tpMapSuperClusters"), cms.InputTag("tpMapGsfElectrons"), cms.InputTag("tpMapIsolation"), cms.InputTag("tpMapId"), cms.InputTag("tpMapHFSuperClusters")),
     # Type of tag-probe candidates, use "Muon" or "Electron"
     # For the moment this only affects the kind of particle
     # used for storing MC truth information.
     tagProbeType = cms.untracked.string('Electron'),
     # Truth Map Tags
-    tagTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch")),
+    tagTruthMatchMapTags = cms.untracked.VInputTag(cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch"), cms.InputTag("HLTMatch")),
     # Store some generic information about the event
     # in case we want it
     mcParticles = cms.untracked.vint32(23, 11, 22),
     trackTags = cms.untracked.VInputTag(cms.InputTag("generalTracks")),
     # Pass Probe Electron Candidate Collections
-    passProbeCandTags = cms.untracked.VInputTag(cms.InputTag("theGsfElectrons"), cms.InputTag("theIsolation"), cms.InputTag("theId"), cms.InputTag("theHLT")),
+    passProbeCandTags = cms.untracked.VInputTag(cms.InputTag("theGsfElectrons"), cms.InputTag("theIsolation"), cms.InputTag("theId"), cms.InputTag("theHLT"), cms.InputTag("HFElectronID")),
     verticesTag = cms.untracked.InputTag("offlinePrimaryVertices"),
-    mcParents = cms.untracked.vint32(0, 0, 0)
+    mcParents = cms.untracked.vint32(0, 0, 0),
+    BestProbeCriteria = cms.untracked.vstring("Random6","OneProbe","OneProbe","OneProbe","Random2"),
+    BestProbeInvMass  = cms.untracked.vdouble(91.1876,91.1876,91.1876,91.1876,91.1876)
 )
 
 process.outpath = cms.OutputModule("PoolOutputModule",
