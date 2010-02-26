f3b2ae2cd595bf150b9b1384acd02d7f097d0ad5
cmsbuild <>
2010-02-26 22:46:46 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- TagAndProbe-CMSSW_3_6_0_pre2'.
    Sprout from master 2010-02-17 19:12:44 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Buildfile conversion to xml'
    Cherrypick from master 2009-10-01 09:43:27 UTC Adam Paul Hunt <sha1-2759bd6d16112107bec24bba4874010a9c4c834c@cern.ch> 'TFileService Addition.  Dev check-in.  DO NOT USE':
        BuildFile
        interface/FCIntervals.hh
        interface/SideBandSubtraction.hh
        plugins/TagProbeEDMAnalysis.cc
        src/FCIntervals.cc
        src/SideBandSubtraction.cc
    Cherrypick from master 2010-02-26 22:46:45 UTC W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch> 'Temporarily resurrecting files to create tag with Framework migration for current integration build':
        interface/ElectronTPValidator.h
        plugins/eTriggerCandProducer.cc
    Cherrypick from PhysicsTools-TagAndProbe-B3_4_X 2010-01-26 11:16:10 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'pick up TFileService from proper place':
        interface/TagProbeEDMAnalysis.h
    Delete:
        interface/BaseTreeFiller.h
        interface/TPTreeFiller.h
        interface/TagProbeFitter.h
        interface/TagProbePairMaker.h
        interface/TriggerCandProducer.h
        plugins/BuildFile
        plugins/DeltaRNearestObjectComputer.cc
        plugins/TagProbeFitTreeAnalyzer.cc
        plugins/TagProbeFitTreeProducer.cc
        plugins/TagProbeFitter.cc
        python/TagProbeFitTreeAnalyzer_cfi.py
        src/BaseTreeFiller.cc
        src/TPTreeFiller.cc
        src/TagProbePairMaker.cc
        src/TriggerCandProducer.icc
        test/Electron_TagProbeTreeFitter_cfg.py
        test/Electron_TagProbeTreeProducer_cfg.py
        test/newstyleFit.py
        test/newstyleWrite.py
        test/oldstyleFit.py
        test/oldstyleNormal.py
        test/oldstyleWrite.py
        test/psetDumpFromTTree.C
        test/testTagProbeFitTreeAnalyzer_JPsi.py
        test/testTagProbeFitTreeFitter_JPsi.py
        test/testTagProbeFitTreeProducer_JPsi.py
        test/testTagProbeFitTreeProducer_ZMuMu.py

--- interface/ElectronTPValidator.h
+++ interface/ElectronTPValidator.h
@@ -34,7 +34,6 @@
 #include "DataFormats/EgammaCandidates/interface/Electron.h"
 #include "DataFormats/EgammaCandidates/interface/ElectronFwd.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
-#include "FWCore/Common/interface/TriggerNames.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 //
 #include "AnalysisDataFormats/Egamma/interface/ElectronIDAssociation.h"
